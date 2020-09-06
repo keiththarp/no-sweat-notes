c1e3ddec2abbe2550f0d2deeab30a76458cdfc90
Keith Tharp <keithstharp@gmail.com>
2020-09-06 15:08:55 -0400

    cleaning up and commenting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,30 @@
+// Require file system from node to write the read/write the DB
 const fs = require('fs');
+// Require uuid to create unique IDs for each note.
 const { v4: uuidv4 } = require('uuid');
 
+// initialize saved notes as a global variable
 let savedNotes = [];
 
+// function to read in the DB to populate our notes list.
 readDB = () => {
   fs.readFile('./db/db.json', (err, data) => {
     if (err) throw err;
     savedNotes = JSON.parse(data);
   });
 };
+
+// start out by reading the DB
 readDB();
 
+// function to overwrite the DB when new data is entered
 rewriteDB = () => {
   fs.writeFile('./db/db.json', JSON.stringify(savedNotes), (err) => {
     if (err) throw err;
   });
 };
 
+// module to send to server
 module.exports = function (app) {
 
   app.get('/api/notes', (req, res) => {
@@ -26,7 +34,10 @@ module.exports = function (app) {
 
   app.post("/api/notes", (req, res) => {
     const thisNote = req.body
-    console.log(thisNote);
+
+    // adding in update functionality.
+
+    // If it's a new note we're going to give it an ID
     if (!thisNote.id) {
       thisNote.id = (uuidv4());
       savedNotes.push(thisNote);
@@ -34,8 +45,8 @@ module.exports = function (app) {
       readDB();
       res.send(savedNotes);
 
+      // otherwise we'll maintain the existing ID and rewrite the note in place.
     } else {
-      const thisID = thisNote.id;
 
       for (let i = 0; i < savedNotes.length; i++) {
         if (savedNotes[i].id === thisNote.id) {
@@ -63,17 +74,3 @@ module.exports = function (app) {
     }
   });
 };
-
-
-
-// * The following API routes should be created:
-// ----------
-//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-// ----------
-//   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the 
-// `db.json` file, and then return the new note to the client.
-// ----------
-//   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
-// This means you'll need to find a way to give each note a unique `id` when it's saved. 
-// In order to delete a note, you'll need to read all notes from the `db.json` file, 
-// remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
